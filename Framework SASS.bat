31157cf85bd8c4bddd7dd180d7f8a9ee00181d58
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-09-18 12:58:32 -0500

    Marbust Technology Company Framework CSS 4.1.6
    /*We added more pixels in font size until 250*/
    /*We added Flex Align Content (.flex-vcc)*/
    /*We added Font Bold (.tbold)*/
    /*We added Min Height in PX and %*/
    /*We added  "flex-direction: row-reverse;"*/
    /*We don't give more support to the scroll bar, you need to apply it to your own css*/
    /*Flex Box Updates with more features according the media queries and flex wraps options*/
    /*------------Main Files-----------------------------------*/
    /* framework.marbust.com/marbust-framework.scss <- SASS*/
    /* framework.marbust.com/marbust-framework.css <- CSS*/
    /*en: This framework it's auto-update, so you can add to your top of your html, or import with the code below, and obtain the new updated features */

---
+++ Framework SASS.bat
@@ -0,0 +1,1 @@
+sass --watch marbust-framework.scss marbust-framework.css
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Marbust Framework</title>
+    <meta http-equiv="refresh" content="1; url=https://framework.marbust.com/marbust-framework.scss">
+</head>
+<body>
+    
+</body>
+</html>
